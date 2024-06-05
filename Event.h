75547b07da0d4d369012b479b8edea8d496a5278
Sameri-dev <142357122+Sameri-dev@users.noreply.github.com>
2024-06-05 11:09:20 -0600

    Create Event.h

---
+++ Event.h
@@ -0,0 +1,21 @@
+#ifndef EVENT_H
+#define EVENT_H
+#include <iostream>
+#include <vector>
+
+#include "Ship.h"
+#include "Astronaut.h"
+
+using namespace std;
+
+class Event
+{
+    public:
+        Event();
+
+        virtual void happen(Ship &nave, vector<Astronaut*> &tripulacion) = 0;
+
+        virtual ~Event();
+};
+
+#endif // EVENT_H
