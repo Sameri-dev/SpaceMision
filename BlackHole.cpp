1ef2808632b05179bad7869b01c635d985b841a0
Sameri-dev <142357122+Sameri-dev@users.noreply.github.com>
2024-06-05 11:05:54 -0600

    Update and rename Investigator.cpp to BlackHole.cpp

---
+++ BlackHole.cpp
@@ -0,0 +1,19 @@
+#include "BlackHole.h"
+
+BlackHole::BlackHole()
+{
+    //ctor
+}
+
+void BlackHole::happen(Ship& nave, vector<Astronaut*>& tripulacion)
+{
+    int adjustDays = (rand()%20) - 10; //num random entre -10 y 10
+
+    cout << "The ship has passed through a black hole, changing mission days to: " << adjustDays << endl;
+}
+
+
+BlackHole::~BlackHole()
+{
+    //dtor
+}
