d32a851d757fb906daa69dcf26b6a19028f5e1dd
Sameri-dev <142357122+Sameri-dev@users.noreply.github.com>
2024-06-09 23:43:29 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -21,12 +21,23 @@ int main()
 {
     srand(time(0)); //num aleatorios
 
-    Engineer ingeniero("Roberto", 100); //creacion de inge1
-    Doctor doctor("Nicole", 100); //creacion de doctor 1
+    cout << "In a universe where people travel through the stars, a small crew is at your command, you must decide their next moves to survive 30 days of travel without the ship's fuel running out." << endl; //intro
+
+    Engineer ingeniero("Robert", 100); //creacion de inge1
+    Doctor doctor("Allan", 100); //creacion de doctor 1
 
     vector<Astronaut*> tripulacion = {&ingeniero, &doctor}; //tripulacion
 
     Ship nave(100); //creacion de la nave
+    Food comida(15); // comida
+    Energy energia(100); //energia
+
+
+     //creacion de eventos aleatorios por medio de un vector
+     vector<Event*> evento;
+     evento.push_back(new Sickness());
+     evento.push_back(new BlackHole());
+     evento.push_back(new BlackOut());
 
     //Para que la partida dure 30 dias
     for (int day = 1; day <= 30; day++){
@@ -36,16 +47,47 @@ int main()
         for(auto& tripulante : tripulacion){
             tripulante -> Status();
         }
-     //creacion de eventos aleatorios
-        int evRandom = rand() % 3; //num random 0-2 (cambiar del 0-4 o mas num para que tenga posibilidad de que no haya nada)
-        Event* evento;
-        if (evRandom == 0) evento = new Sickness();
-        else if (evRandom == 1) evento = new BlackOut();
-        else evento = new BlackHole();
 
-        evento -> happen(nave, tripulacion);
+        //le damos la opcion de esocger su siguiente movimiento al jugador
+        bool contin = false;
+        while(!contin){
+                //opciones
+            cout << "Whats your next move? " << endl;
+            cout << endl << "1. Eat" << endl;
+            cout << "2. Healing" << endl;
+            cout << "3. Repair ship" << endl;
+            cout << "4. Endure" << endl;
 
-        delete evento;
+                int opcion;
+                cin >> opcion;
+
+                switch (opcion){
+                case 1:
+                    for(auto& tripulante : tripulacion){
+                        comida.use(*tripulante);
+                    }
+                    break;
+
+                case 2:
+                    doctor.interact(tripulacion);
+                    break;
+
+                case 3:
+                    ingeniero.interact(tripulacion, nave);
+                    break;
+
+                case 4:
+                    contin = true;
+                    break;
+
+                default:
+                    cout << "Not a valid option . . . Try again" << endl;
+            }
+        }
+
+        //uso de los eventos random
+        int evRandom = rand() % 4; //num random 0-2 (cambiar del 0-4 o mas num para que tenga posibilidad de que no haya nada)
+        evento[evRandom]->happen(nave, tripulacion);
 
         cout << ".     .     ." << endl;
     }
