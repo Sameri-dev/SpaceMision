5b9ffb67abd830170101e41fb918ef7adfcec298
Sameri-dev <142357122+Sameri-dev@users.noreply.github.com>
2024-06-09 23:51:18 -0600

    Update Food.cpp

--- Food.cpp
+++ Food.cpp
@@ -5,9 +5,21 @@ Food::Food(int amount) : Supply(amount)
 
 }
 
-void Food::use()
+void Food::use(Astronaut &astronauta)
 {
-    cout << "Using" << amount << "of food" << endl;
+    if(amount > 0){
+        amount --;
+
+        int newHealth = astronauta.getHealth() + 30;
+        if(newHealth > 100){
+            newHealth = 100;
+        }
+
+        astronauta.setHealth(newHealth);
+        cout << "Using 1 unit of food...Healt of crew member: " << astronauta.getHealth() << "%" << endl;
+    } else {
+        cout << "There is no food left..." << endl;
+    }
 }
 
 Food::~Food()
