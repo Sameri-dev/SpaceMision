d5808c852de7d4f7d504027a9499145240883bf4
Sameri-dev <142357122+Sameri-dev@users.noreply.github.com>
2024-06-09 23:48:42 -0600

    Update Energy.cpp

--- Energy.cpp
+++ Energy.cpp
@@ -5,6 +5,27 @@ Energy::Energy(int amount) : Supply(amount)
 
 }
 
+void Energy::use(Ship &nave)
+{
+    if(amount > 0){
+        amount --;
+        nave.adjustEnergy(30);
+    }
+    cout << "Using: " << amount << "energy" << endl;
+    cout << "Ship's energy upto: " << nave.getEnergy << endl;
+}
+
+Energy::~Energy()
+{
+    //dtor
+}
+#include "Energy.h"
+
+Energy::Energy(int amount) : Supply(amount)
+{
+
+}
+
 void Energy::use()
 {
     cout << "Using: " << amount << "energy" << endl;
