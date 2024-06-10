f68cb93f92266664dd2ece4e5a4aea9662b47326
Sameri-dev <142357122+Sameri-dev@users.noreply.github.com>
2024-06-09 23:50:00 -0600

    Update Engineer.h

--- Engineer.h
+++ Engineer.h
@@ -1,8 +1,9 @@
 #ifndef ENGINEER_H
 #define ENGINEER_H
 #include <iostream>
+#include <vector>
 #include "Astronaut.h"
-
+#include "Ship.h"
 using namespace std;
 
 class Engineer : public Astronaut
@@ -10,10 +11,7 @@ class Engineer : public Astronaut
     public:
         Engineer(string name, int health);
 
-        void interact() override;
-
-        virtual ~Engineer();
-
+        void interact(Ship &nave);
 };
 
 #endif // ENGINEER_H
