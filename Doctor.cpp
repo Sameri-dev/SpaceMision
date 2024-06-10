af9fa2bc2289c85830c581611358f327c812d898
Sameri-dev <142357122+Sameri-dev@users.noreply.github.com>
2024-06-09 23:47:46 -0600

    Update Doctor.cpp

--- Doctor.cpp
+++ Doctor.cpp
@@ -2,16 +2,21 @@
 
 Doctor::Doctor(string name, int health) : Astronaut(name, health)
 {
+}
+
+void Doctor::interact(vector<Astronaut*> &tripulacion)
+{
     cout << name << "healing the crew" << endl;
-}
 
-void Doctor::interact()
-{
+    for (auto& tripulante : tripulacion){
+        if (tripulante != this){
+            int newHealth = tripulante -> getHealth() + 20;
+            if (newHealth > 100){
+                newHealth = 100;
+            }
 
-}
-
-
-Doctor::~Doctor()
-{
-    //dtor
+            tripulante -> setHealth(newHealth);
+        }
+    }
+    this -> setHealth(this->getHealth()-10);
 }
