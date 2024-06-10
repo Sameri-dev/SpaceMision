44959a1420348fb8b195f996471bfca2e8fc3919
Sameri-dev <142357122+Sameri-dev@users.noreply.github.com>
2024-06-09 23:50:23 -0600

    Update Event.cpp

--- Event.cpp
+++ Event.cpp
@@ -1,12 +1,6 @@
 #include "Event.h"
 
-void Event::happen(Ship& nave, vector<Astronaut*>& tripulacion)
+void Event::happen(Ship &nave, vector<Astronaut*>& tripulacion)
 {
 
-}
-
-
-Event::~Event()
-{
-    //dtor
 }
